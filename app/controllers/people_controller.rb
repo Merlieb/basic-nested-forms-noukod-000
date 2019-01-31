2ed22325e12595cbef236a00fa4d63044b39b9a9
Merline Belizaire <merline@noukod.com>
2019-01-31 19:02:21 +0000

    Automatically backed up by Learn

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,16 +4,16 @@ class PeopleController < ApplicationController
     @person.addresses.build(address_type: 'work')
     @person.addresses.build(address_type: 'home')
   end
- 
+
   def create
     person = Person.create(person_params)
     redirect_to people_path
   end
- 
+
   def index
     @people = Person.all
   end
- 
+
   private
 
   def person_params
